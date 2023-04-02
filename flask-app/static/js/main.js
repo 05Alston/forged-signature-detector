11382de92a8d3e694745e55b6bc70e12c2371ac9
05Alston <0508alstonfernandes@gmail.com>
2023-04-02 11:04:32 +0530

    update structure

--- flask-app/static/js/main.js
+++ flask-app/static/js/main.js
@@ -1,33 +1,60 @@
 $(document).ready(function () {
-    // // Init
-    // $('.image-section').hide();
-    // $('.loader').hide();
-    // $('#result').hide();
+    // Init
+    // $('.img-preview1').hide();
+    // $('.img-preview2').hide();
+    // $('#btn-predict').hide();
+    $('.loader').hide();
+    $('#result').hide();
 
-    // // Upload Preview
-    // function readURL(input) {
-    //     if (input.files && input.files[0]) {
-    //         var reader = new FileReader();
-    //         reader.onload = function (e) {
-    //             $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-    //             $('#imagePreview').hide();
-    //             $('#imagePreview').fadeIn(650);
-    //         }
-    //         reader.readAsDataURL(input.files[0]);
-    //     }
-    // }
-    // $("#imageUpload").change(function () {
-    //     $('.image-section').show();
-    //     $('#btn-predict').show();
-    //     $('#result').text('');
-    //     $('#result').hide();
-    //     readURL(this);
-    // });
-
+    // Upload Preview
+    function readURL1(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview1').css('background-image', 'url(' + e.target.result + ')');
+                // $('#imagePreview1').hide();
+                $('#imagePreview1').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    function readURL2(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview2').css('background-image', 'url(' + e.target.result + ')');
+                // $('#imagePreview2').hide();
+                $('#imagePreview2').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload1").change(function () {
+        $('.image-preview1').show();
+        // $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL1(this);
+        if($(".img-preview1").is(":visible") && $(".img-preview2").is(":visible")){
+            $('#btn-predict').show();
+        }
+    });
+    $("#imageUpload2").change(function () {
+        console.log('shit')
+        $('.image-preview2').show();
+        // $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL2(this);
+        if($(".img-preview1").is(":visible") && $(".img-preview2").is(":visible")){
+            $('#btn-predict').show();
+        }
+    });
+    
     // Predict
     $('#btn-predict').click(function () {
         var form_data = new FormData($('#upload-file')[0]);
-
+        console.log(form_data)
         // Show loading animation
         $(this).hide();
         $('.loader').show();
